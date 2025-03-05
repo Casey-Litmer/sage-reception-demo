87f0241a0fa38ea1454d2d26a161977998361ba5
Casey Litmer <litmerc@msn.com>
2025-03-05 00:14:19 -0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "./*.tsx",
+    "./*.ts",
+    "./**/*.tsx",
+    "./**/*.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
