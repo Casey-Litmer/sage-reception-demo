94647d5e805b00655cb23fcdb7a5b3f23bf81b13
Casey Litmer <litmerc@msn.com>
2025-03-06 01:01:38 -0800

    SMS user interface

--- src/components/taskViews/SMS.tsx
+++ src/components/taskViews/SMS.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SMS.css';
+import { Button } from 'flowbite-react';
+import { CiPaperplane } from "react-icons/ci";
 
 export default function SMSComponent() {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [response, setResponse] = useState('');
+    
+    const handleSubmit = (ev:any) => {
+        ev.preventDefault();
+        
+    };
+    const handleNumber = (ev: any) => setPhoneNumber(ev.target.value);
+    const handleResponse = (ev: any) => setResponse(ev.target.value);
+    
     return (
-        <div className='SMS'>
-
-        </div>
+        <form onSubmit={handleSubmit} className=' space-y-2 SMS'>
+            <div>Client's Phone Number:</div>
+            <input
+                type='text'
+                onChange={handleNumber}
+                className='w-2/3'
+            />
+            <div>Your Response:</div>
+            <textarea
+                onChange={handleResponse}
+                className='w-2/3 Input'
+            />
+            <Button type='submit' color={'black'} className='border-2'>
+                <p style={{fontSize:24, textAlign:'center'}}>Send</p>
+                <CiPaperplane size={32} color='var(--color-middle)'/>
+            </Button>
+        </form>
     );
 };
