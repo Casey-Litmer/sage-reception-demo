fd56efea5fba3efd661b60e70045405900425f15
Casey Litmer <litmerc@msn.com>
2025-03-07 16:47:33 -0800

    Added spring on PatienceBar

---
+++ src/components/messageBox/MessageBox.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import './MessageBox.css';
+import { Message } from '../../types';
+import { IconButton } from '@mui/material';
+import { ChevronLeft, ChevronRight, Warning} from '@mui/icons-material';
+import { useMessages } from '../../contexts/MessageProvider';
+import PatienceBar from './PatienceBar';
+
+
+
+export default function MessageBox() {
+    const {pendingMessages} = useMessages();
+
+    const items = pendingMessages.map(ms => {
+        const key = ms.message.messageTime.toString();
+        const message = ms.message;
+        const remainingTime = ms.remainingTime;
+        return <MessageContainer key={key} message={message} remainingTime={remainingTime}/>
+    });
+
+    return (
+        <div className='BoxOutline MessageBox'>
+            {items}
+        </div>
+    );
+};
+
+//============================================================================
+interface MessageContainerProps {
+    message: Message;
+    remainingTime: number;
+};
+export function MessageContainer(props: MessageContainerProps) {
+    const {message, remainingTime} = props;
+    const [open, setOpen] = useState(false);
+    
+    //============================================================================
+    const messageTime = message.messageTime;
+    const timerBarPercent = remainingTime / message.timeToRespond * 100;
+    const formattedTime = `${messageTime.getHours()}:${messageTime.getMinutes()}:${messageTime.getSeconds()}`
+    const info = `[${formattedTime}]: ${message.author} (${message.phoneNumber})`;
+    const urgent = message.timeToRespond < 60000; 
+
+    //============================================================================
+    const handleOpen = () => setOpen(o => !o);
+
+    //============================================================================
+    return (
+        <div className='BoxOutline MessageContainer'>
+            <div style={{flexDirection:'row', display:'flex', alignItems:'center'}}>
+                <IconButton id='View Message'
+                    onClick = {handleOpen}
+                >
+                    {open ? <ChevronLeft/> : <ChevronRight/>}
+                </IconButton>
+
+                <text>{info}</text>
+
+                {urgent && 
+                <div className='absolute right-8' 
+                    style={{color:'var(--color-middle)'}}>
+                    Urgent
+                    <Warning/>
+                </div>}
+            </div>
+
+            <PatienceBar timerBarPercent={timerBarPercent}/>
+
+            {open && 
+                <div className='DropDownContainer'>
+                    <div className='InnerContainer'>
+                        {message.message}
+                    </div>
+                </div>
+            }
+        </div>
+    );
+};
---
+++ src/components/messageBox/PatienceBar.tsx
@@ -0,0 +1,25 @@
+import { useSpring, animated } from '@react-spring/web';
+
+const AnimatedDiv = animated("div");
+
+interface PatienceBarProps {
+    timerBarPercent: number;
+}
+export default function PatienceBar(props: PatienceBarProps) {
+    const {timerBarPercent} = props;
+
+    const barSpring = useSpring({
+        config: {duration: 1000},
+        to:{width:`${timerBarPercent}%`}
+    });
+
+    return (
+        <div className='h-2.1 w-full bg-background' style={{
+            borderTop: 1, borderBottom:1, 
+            borderStyle:'solid', borderColor: 'var(--color-top)'
+        }}>
+            <AnimatedDiv className='bg-middle h-2' id='TimerBar'
+                style={barSpring}/>
+        </div>
+    );
+};
