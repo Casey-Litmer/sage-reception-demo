def6ff412fed541c1cad23509ce62f0e319ca799
Casey Litmer <litmerc@msn.com>
2025-03-10 15:24:46 -0700

    Fixed header height not working

--- src/components/PageContents.tsx
+++ src/components/PageContents.tsx
@@ -8,7 +8,13 @@ export default function PageContents(props: PageContentsProps) {
     const {children} = props;
   
     return (
-        <div className='items-center flex flex-col w-full h-full pt-8'>
+        <div 
+            className='items-center flex flex-1 flex-col w-full h-full pt-8'
+            //style={{
+            //    flex: 1, flexDirection: 'column', width: '100%', height: '100%',
+            //    paddingTop: '32px', alignItems: 'center', justifyItems:'center'
+            //}}
+        >
             {children}
         </div>
   );
